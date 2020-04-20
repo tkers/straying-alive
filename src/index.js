945d3d35e282cdeb306cce7d35c4cd462ab26e16
Tijn Kersjes <tijn@divbyzero.nl>
2020-04-20 02:13:03 +0200

    Add pause feature

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 import { createGame } from "./game.js";
 
+let running = true;
 const gameLoop = cb => {
   let pt = 0;
   const tick = t => {
     const dt = (t - pt) / 1000;
     pt = t;
-    cb(dt);
+    running && cb(dt);
     requestAnimationFrame(tick);
   };
 
   tick(0);
 };
+
+window.addEventListener("keydown", e => {
+  if (e.key === " ") {
+    running = !running;
+    document.getElementById("root").style.opacity = running ? 1 : 0.5;
+  }
+});
 
 window.addEventListener("load", () => {
   const game = createGame(document.getElementById("root"));
