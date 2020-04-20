e9ce4f15bdf82c905bd2762c116fd598e4bddab7
Tijn Kersjes <tijn@divbyzero.nl>
2020-04-20 20:53:45 +0200

    Improve gameloop handling

--- src/index.js
+++ src/index.js
@@ -1,26 +1,33 @@
 import { createGame } from "./game.js";
 
-let running = true;
 const gameLoop = cb => {
+  let isPaused = false;
   let pt = 0;
   const tick = t => {
     const dt = (t - pt) / 1000;
     pt = t;
-    running && cb(dt);
+    !isPaused && cb(dt);
     requestAnimationFrame(tick);
   };
 
   tick(0);
+
+  return {
+    pause: () => {
+      isPaused = !isPaused;
+      return isPaused;
+    }
+  };
 };
 
-window.addEventListener("keydown", e => {
-  if (e.key === " ") {
-    running = !running;
-    document.getElementById("root").style.opacity = running ? 1 : 0.5;
-  }
-});
-
 window.addEventListener("load", () => {
-  const game = createGame(document.getElementById("root"));
-  gameLoop(game.update);
+  const root = document.getElementById("root");
+  const game = createGame(root);
+  const loop = gameLoop(game.update);
+
+  window.addEventListener("keydown", e => {
+    if (e.key === " ") {
+      root.style.opacity = loop.pause() ? 0.5 : 1;
+    }
+  });
 });
